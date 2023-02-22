6c9c744d6de1748ddbe0fb1ac0582319ded28ee9
256k <fayek.helmi@gmail.com>
2023-02-21 22:12:23 -0500

    wip

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 console.log("hello runes")
 
+// let's treat this like a norns / lua file but in js
+
+const STEPS = 8;
+const tracks = [];
+const PARAMSKEYS = ["step", "note", "oct", "prob", "clkdiv" ];
+
+// class track {
+//     constructor(step, note, octave, probability, clkdiv) {
+//       this.step = step;
+//       this.note = note;
+//       this.octave = octave;
+//       this.probability = probability;
+//       this.clkdiv = clkdiv;
+//     }
+
+//     draw() {
+//         // draw stuff
+//     }
+//   }
+  
+let track = {step: [], note:[], oct: [], prob:[], clkdiv: []}
+
+
+for (let param = 0; param < Object.keys(track).length; param++) {
+    for (let stepIndex = 0; stepIndex < STEPS; stepIndex++) {
+        track[PARAMSKEYS[param]][stepIndex] = "|";
+    }
+}
+track.step[2] = 0;
+track.step[5] = 0;
+track.step[7] = 0;
+
+function runStep(trackStep) {
+    if(track.step[trackStep] === "O") {
+        
+    }
+}
+
+console.log(track)
